cf56baea31891cc0fda78c14404c8fd846e5d9e8
Pr0K0Rrastinat <zhasulanaldiiar@gmail.com>
2025-05-04 23:08:44 +0600

    fix bug in result/by-email added file_uuid

--- backend/routes/results.py
+++ backend/routes/results.py
@@ -40,13 +40,21 @@ def get_result_by_uuid(file_uuid: str, db: Session = Depends(get_db)):
 @router.get("/result/by-email/{user_email}")
 async def get_results_by_email(user_email: str, db: Session = Depends(get_db)):
     results = db.query(Result).filter(Result.user_email == user_email).all()
-    
+
     if not results:
         raise HTTPException(status_code=404, detail="No results or files found for this email")
 
     return {
         "email": user_email,
-        "results": [{"filename": result.filename, "result": result.result,"filetype":result.filetype} for result in results]
+        "results": [
+            {
+                "filename": result.filename,
+                "result": result.result,
+                "filetype": result.filetype,
+                "file_uuid": result.file_uuid,
+            }
+            for result in results
+        ],
     }
 
 @router.post("/result/delete/{user_email}/{fileUuid}", status_code=status.HTTP_204_NO_CONTENT)
